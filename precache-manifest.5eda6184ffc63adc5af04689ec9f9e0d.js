ad612d076722bdb5b1648b81db9d878aaf43bf9e
Seymur <mustafav.seymur@gmail.com>
2019-11-27 14:20:35 +0200

    Updates

---
+++ precache-manifest.5eda6184ffc63adc5af04689ec9f9e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f59f54827014f7eec69088a4e9cad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a370d2901198e3bd6f9c",
+    "url": "/static/css/main.64f6e06d.chunk.css"
+  },
+  {
+    "revision": "ce08e30413ace01fd0bb",
+    "url": "/static/js/2.f2e308a4.chunk.js"
+  },
+  {
+    "revision": "a370d2901198e3bd6f9c",
+    "url": "/static/js/main.b557559f.chunk.js"
+  },
+  {
+    "revision": "268accc83a7dcdfbb9fe",
+    "url": "/static/js/runtime-main.3aa6b27e.js"
+  }
+]);
